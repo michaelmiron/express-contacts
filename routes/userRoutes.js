f92189b2d1fe980ba1a7305c376433ac4fce97c5
michael_miron <micha767a@gmail.com>
2025-07-23 09:38:30 +0300

    add JWT token validation middleware

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,11 +4,12 @@ const {
     loginUser,
     currentUser
 } = require("../controllers/userController");
+const validateToken = require("../middleware/validatTokenHandler");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/loginUser", loginUser);
-router.get("/current", currentUser);
+router.get("/current", validateToken, currentUser);
 
 module.exports = router;
